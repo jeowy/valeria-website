08cba3a38b2551ff3e7c4cf47980e6cbc2558f2d
jeowy <jeowy@hotmail.co.uk>
2026-04-29 12:46:19 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://username.github.io',
+  site: 'https://jeowy.github.io',
   base: '/valeria-website',
   output: 'static'
 });
